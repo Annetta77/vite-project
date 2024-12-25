8bcf15b78ab1df1ba0cafacb5f1e8d3abf9751cd
Anna <ania.ania.1996@mail.ru>
2024-12-25 19:40:11 +0300

    a

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Greeting from './components/Greeting.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <Greeting name="Мария" />
+      <Greeting name="Александр" />
+    </div>
+  )
+}
+
+export default App;
